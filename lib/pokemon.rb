f55a677a75a311ceab8e264019248b578714c8ba
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-11 19:38:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,5 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
-<<<<<<< HEAD
 
   def initialize(id = nil)
     @id = id
@@ -34,27 +33,3 @@ class Pokemon
       pokemon
     end
   end
-=======
-  @@all = []
-
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
-
-  def self.save(pk_name, pk_type, db)
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type, db)
-      VALUES (?, ?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.type, self.db)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-  end
-
-  def self.all
-    @@all
-  end
-
-
-end
->>>>>>> f127463bd9f4b5234704a7a27dea2e9ac4c1c51d
