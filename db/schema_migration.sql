b40d0f32003f745326420c303853d19403c4af4a
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-11 19:36:08 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,4 +1,10 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
 
+<<<<<<< HEAD
+=======
+-- sqlite3 path/to/pokemon.db < Pokemon.save
+
+-- sqlite3 path/to/database.db < source_of_sql_data
+>>>>>>> f127463bd9f4b5234704a7a27dea2e9ac4c1c51d
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
